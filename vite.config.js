79d615e068d43a2cabb1f3fa4981b1f47b79c5bd
cactus <155895053+xdc7-css@users.noreply.github.com>
2025-09-24 23:24:47 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfoilo/'  // <--- هذا مهم جداً
+  base: '/portfolio/',   // 👈 لازم يطابق اسم الريبو الجديد
 })
